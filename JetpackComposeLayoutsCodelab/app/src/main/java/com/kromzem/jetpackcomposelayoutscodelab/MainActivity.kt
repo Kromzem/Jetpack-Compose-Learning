3163271ec1561f2aab73b0876f9639cfc579762b
Kromzem <kromzem.games@gmail.com>
2021-03-17 13:06:46 +0100

    progress

--- JetpackComposeLayoutsCodelab/app/src/main/java/com/kromzem/jetpackcomposelayoutscodelab/MainActivity.kt
+++ JetpackComposeLayoutsCodelab/app/src/main/java/com/kromzem/jetpackcomposelayoutscodelab/MainActivity.kt
@@ -3,11 +3,28 @@ package com.kromzem.jetpackcomposelayoutscodelab
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kromzem.jetpackcomposelayoutscodelab.ui.theme.JetpackComposeLayoutsCodelabTheme
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +34,7 @@ class MainActivity : AppCompatActivity() {
             JetpackComposeLayoutsCodelabTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    PhotographerCard()
                 }
             }
         }
@@ -25,14 +42,95 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun PhotographerCard() {
+    Row {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            //Image goes here
+        }
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun LayoutsCodelab() {
+    val favorite = remember { mutableStateOf(false) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutsCodelab")
+                },
+                actions = {
+                    FavoriteButton(isFavorite = favorite.value, onFavoriteChanged = { favorite.value = it })
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun FavoriteButton(isFavorite: Boolean, onFavoriteChanged: (Boolean) -> Unit) {
+    IconButton(onClick = { onFavoriteChanged(!isFavorite) }) {
+        Icon(
+            if (isFavorite)
+                Icons.Filled.Favorite
+            else
+                Icons.Outlined.FavoriteBorder,
+            contentDescription = null
+        )
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column {
+        Column(modifier = modifier) {
+            Text(text = "Hi there")
+            Text(text = "Thanks for going through the Layouts codelab")
+        }
+        SimpleList(modifier)
+    }
+}
+
+@Composable
+fun SimpleList(modifier: Modifier = Modifier) {
+    val scrollState = rememberLazyListState()
+    
+    LazyColumn(state = scrollState) {
+        items(100) {
+            Text(text = "Item #$it")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview() {
     JetpackComposeLayoutsCodelabTheme {
-        Greeting("Android")
+        PhotographerCard()
+    }
+}
+
+@Preview
+@Composable
+fun LayoutsCodelabPreview() {
+    JetpackComposeLayoutsCodelabTheme {
+        LayoutsCodelab()
     }
 }
