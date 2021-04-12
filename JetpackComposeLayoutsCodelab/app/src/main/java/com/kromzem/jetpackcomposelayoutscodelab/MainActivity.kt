3b2e3831fe913aff4b52212bb0fe24c99273a57d
Kromzem <kromzem.games@gmail.com>
2021-04-12 22:04:17 +0200

    complex custom layout

--- JetpackComposeLayoutsCodelab/app/src/main/java/com/kromzem/jetpackcomposelayoutscodelab/MainActivity.kt
+++ JetpackComposeLayoutsCodelab/app/src/main/java/com/kromzem/jetpackcomposelayoutscodelab/MainActivity.kt
@@ -3,10 +3,15 @@ package com.kromzem.jetpackcomposelayoutscodelab
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
@@ -14,6 +19,7 @@ import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.FirstBaseline
 import androidx.compose.ui.layout.Layout
@@ -25,6 +31,7 @@ import androidx.compose.ui.unit.dp
 import com.kromzem.jetpackcomposelayoutscodelab.ui.theme.JetpackComposeLayoutsCodelabTheme
 import dev.chrisbanes.accompanist.coil.CoilImage
 import kotlinx.coroutines.launch
+import kotlin.math.max
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -178,6 +185,91 @@ fun MyOwnColumn(
     }
 }
 
+@Composable
+fun StaggeredGrid(
+    modifier: Modifier = Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit
+) {
+    Layout(content = content, modifier = modifier) { measureable, constraints ->
+        val rowWidths = IntArray(rows) { 0 }
+        val rowMaxHeights = IntArray(rows) { 0 }
+
+        val placeables = measureable.mapIndexed { index, measurable ->
+            val placeable = measurable.measure(constraints)
+
+            val row = index % rows
+            rowWidths[row] = rowWidths[row] + placeable.width
+            rowMaxHeights[row] = max(rowMaxHeights[row], placeable.height)
+
+            placeable
+        }
+
+        val width = rowWidths.maxOrNull()?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
+        val height = rowMaxHeights.sum().coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i - 1] + rowMaxHeights[i - 1]
+        }
+
+        layout(width, height) {
+            val rowX = IntArray(rows) { 0 }
+
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+
+                rowX[row] += placeable.width
+            }
+        }
+    }
+}
+
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(16.dp)
+                    .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = text)
+        }
+    }
+}
+
+
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
+@Composable
+fun ChipGrid(modifier: Modifier = Modifier){
+    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
+        StaggeredGrid {
+            for (topic in topics) {
+                Chip(Modifier.padding(8.dp), text = topic)
+            }
+        }
+    }
+}
+
 @Preview
 @Composable
 fun PhotographerCardPreview() {
@@ -220,5 +312,21 @@ fun MyOwnColumnInAction() {
             Text("vertically.")
             Text("We've done it by hand!")
         }
+    }
+}
+
+@Preview
+@Composable
+fun ChipPreview(){
+    JetpackComposeLayoutsCodelabTheme(){
+        Chip(text = "Hello there :)")
+    }
+}
+
+@Preview
+@Composable
+fun ChipGridPreview() {
+    JetpackComposeLayoutsCodelabTheme {
+        ChipGrid()
     }
 }
